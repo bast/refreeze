5d88ee6cf3876efbfd6f6fa09bde3b337b1d788e
Olav Vahtras <vahtras@kth.se>
2018-04-13 14:22:47 +0200

    open directly in browser

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from jinja2 import Template
+import webbrowser
 
 app = Flask(__name__, template_folder='.', static_url_path='', static_folder='..')
 app.config.from_pyfile('settings.py')
@@ -24,4 +25,5 @@ def home():
 if __name__ == '__main__':
     BASE = ''
     port = int(os.environ.get('PORT', 5000))
+    webbrowser.open_new_tab('http://localhost:{}'.format(port))
     app.run(host='0.0.0.0', port=port)
